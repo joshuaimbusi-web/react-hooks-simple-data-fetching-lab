c7c28431226d6c2611edcfa5a8cd984ac30cb617
joshuaimbusi <joshua.imbusi@student.moringaschool.com>
2025-10-20 21:33:54 +0300

    "initial commit"

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching the dog image:", error);
+      });
+  }, []);
+
+  if (!imageUrl) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={imageUrl} alt="A Random Dog" />;
+}
+
+export default App;
+
